90f94f03a2ed9f3f30b251a0ffaad2c1282d4993
Sergiy Povzhyk <sergiypovzhyk@gmail.com>
2023-03-09 18:08:49 +0200

    create checkable fragment

--- app/src/main/java/com/example/quakealertapp/ui/CheckableFragment.kt
+++ app/src/main/java/com/example/quakealertapp/ui/CheckableFragment.kt
@@ -5,17 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import com.example.quakealertapp.R
 
-class AboutFragment : Fragment() {
+class CheckableFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_about, container, false)
-
+        return inflater.inflate(R.layout.fragment_checkable, container, false)
     }
 }
